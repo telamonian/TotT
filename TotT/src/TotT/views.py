1cf2c14cdade8df151a17eefc09587f75a39dc8a
Mariusz Matyszewski <matyszm@gmail.com>
2016-02-06 15:15:17 -0500

    reusable view on search

--- TotT/src/TotT/views.py
+++ TotT/src/TotT/views.py
@@ -14,26 +14,38 @@ class AboutPage(generic.TemplateView):
 
 # views added by JRJ to develop further
 class SearchPage(generic.TemplateView):
+    search_type = None
     template_name = "search.html"
 
-    '''def form_valid(self, form):
-        return render(request, 'search.html', {'words': form.sub_words,})'''
+    '''def get_object(self, queryset=None):
+        return queryset.get(search_type=self.search_type)'''
+
+    def get_context_data(self, **kwargs):
+        context = None
+        if self.search_type=="Gifs":
+            context = {
+                'gifs': 1,
+                }
+        else:
+            context = {
+                'gifs': 0
+            }
+        return context
+
     def post(self, request, *args, **kwargs):
         words = WordForm(request.POST)
         if words.is_valid():
-            gif=GetGifInfo()
-            q=gif.make_query_simple(words.get_list()[0])
-            gif.get_json_object(q)
-            imgDat=gif.get_gif_url_original_size_one(0)
-            return render(request, 'search.html', {'gif': imgDat,})
+            if self.search_type=="Gifs":
+                gif=GetGifInfo()
+                q=gif.make_query_simple(words.get_list()[0])
+                gif.get_json_object(q)
+                imgDat=gif.get_gif_url_original_size_one(0)
+                return render(request, 'search.html', {'gif': imgDat,})
+            else:
+                return render(request, 'search.html', {'words': words.get_list(),})
         else:
             return render(request, 'search.html', {'error_message': "Please type in some words",})
-'''
-        #if(request.POST.get('search_thesaurus')):
-            some_val = 'did it stick? {0}'.format(request.GET.get('search_words'))
-            return HttpResponse('results')
-        else:
-            return HttpResponse('search')'''
+
 
 class ResultsPage(generic.TemplateView):
     template_name = "result.html"
